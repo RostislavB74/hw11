30495e2d1a71f625dd1d3522c4ace0a2be406827
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-11-11 21:39:47 +0200

    ok

---
+++ src/schemas.py
@@ -0,0 +1,32 @@
+from datetime import date, datetime
+
+from pydantic import BaseModel, Field, EmailStr
+
+class ContactBase(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: str
+    birth_date: date
+    additional_data: str = None
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class ContactResponse(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: str
+    birth_date: date
+    additional_data: str = None
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
+        
